de1e50daeb6ed5fefc64e4ee77a14293f52ac300
Julio Faerman <jufaerma@redhat.com>
2022-05-26 16:34:47 +0000

    Initial cloud formation resource type

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cfn-ocp
+
 CloudFormation components for OpenShift
+
+# Resources
+
+https://docs.aws.amazon.com/cloudformation-cli/latest/userguide/resource-type-walkthrough.html
---
+++ src/main/java/openshift/ocp/cluster/Configuration.java
@@ -0,0 +1,8 @@
+package openshift.ocp.cluster;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("openshift-ocp-cluster.json");
+    }
+}
